031e39a83efce9e2ed6fc8aa638ca3be6c475316
Altynbek38 <altynbek4649@gmail.com>
2025-04-19 15:54:33 +0500

    finished pre debug

--- src/main/java/com/example/qwartz_scheduler/payload/EmailRequest.java
+++ src/main/java/com/example/qwartz_scheduler/payload/EmailRequest.java
@@ -1,8 +1,33 @@
 package com.example.qwartz_scheduler.payload;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Setter
+@Getter
 public class EmailRequest {
 
-//    @Email
+    @Email
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String subject;
+
+    @NotEmpty
+    private String body;
+
+    @NotNull
+    private LocalDateTime dateTime;
+
+    @NotNull
+    private ZoneId timeZone;
+
+
 }
